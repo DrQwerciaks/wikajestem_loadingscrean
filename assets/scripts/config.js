0dee1d8c62c09ec9e3121acd20b09591de55c73d
DrQwerciak <77661984+DrQwerciaks@users.noreply.github.com>
2023-11-07 20:10:33 +0100

    wika loading

---
+++ assets/scripts/config.js
@@ -0,0 +1,29 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "projectlife.wloff.pl:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Zapraszamy na nasz serwer Discord!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/invite/Nrj8nNSUJ3"},
+    {name: "tiktok", label: "Tiktok", description: "Zapraszamy na naszego TikToka!", icon: "assets/media/icons/tiktok.png", link: "https://www.tiktok.com/muffinguard.net"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "`wikajestem#3517", description: "Założycielka", color: "#6d0000", image: "https://cdn.discordapp.com/avatars/1064206728041857084/630623137203032708145b837d0ff565.png?size=1024"},
+    {name: "!DrQwerciak#2137", description: "Menadżer", color: "#1dac3d", image: "https://cdn.discordapp.com/avatars/979989714272681984/a_628cb2a798aa27de1afd2bd87047734f.gif?size=1024"},
+    {name: "aditzu10_", description: "Menadżer", color: "#1dac3d", image: "https://cdn.discordapp.com/avatars/622123608600477737/557c3421379f5cce8499005ddff9c00a.png?size=1024"},
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "TEAM", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+author '`wikajestem#3517'
+description 'Loading by `wikajestem#3517 https://discord.gg/PwmG4epCvq'
+version '0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
